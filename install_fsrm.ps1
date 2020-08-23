6f2413f48f870f5d057fc3bea6c10c3c19faa36f
AlissonBetim <alissonbetim@gmail.com>
2020-08-23 01:36:42 -0300

    okok

--- install_fsrm.ps1
+++
@@ -1,6 +0,0 @@
-Install-WindowsFeature –Name FS-Resource-Manager –IncludeManagementTools -Restart
-#Cria Lista - Caso de ok segue senão reinicia maquina pois precisa da segunda reinicialização
-$exec=new-FsrmFileGroup -name "Arquivos Ransomware" -IncludePattern @((Invoke-WebRequest -UseBasicParsing -Uri "https://fsrm.experiant.ca/api/v1/get").content | convertfrom-json | % {$_.filters})
-if ($exec)
-{}
-else {Restart-Computer}
